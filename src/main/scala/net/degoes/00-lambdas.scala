37b88088d8d0e68713be86921c874b82012f1407
sujeenoh-dev <sujeenoh@devsisters.com>
2025-07-15 20:29:42 +0900

    translate 00-lambdas

--- src/main/scala/net/degoes/00-lambdas.scala
+++ src/main/scala/net/degoes/00-lambdas.scala
@@ -1,15 +1,16 @@
 /**
- * Functions are fundamental to functional programming. In a functional
- * programming language, functions are values, which may be stored in
- * fields, accepted as arguments to other functions, and returned from
- * functions. So-called "first-class functions" go by many names in
- * different programming languages, including "closures", "lambdas",
- * and "anonymous functions". Effectively understanding and using
- * functions is a pre-requisite to the mastery of functional
- * programming, in Scala or any other programming language.
- *
- * In this module, you will learn about "functions as values" and become
- * comfortable creating, transforming, composing, and typing functions.
+ * # 람다란 무엇인가?
+ * 람다 혹은 람다 표현식은 익명 함수를 가리키는 용어입니다. 즉, 이름 없이 즉석에서 정의하는 함수를 뜻합니다.
+ * 스칼라에서는 아래와 같이 생성할 수 있습니다.
+ * ```scala
+ * val square: Int => Int = (x: Int) => x * x
+ * ```
+ * 람다는 함수 타입을 가지며, 위 예시에서는 `Int => Int` 타입을 가집니다.
+ * 
+ * 함수는 함수형 프로그래밍의 근간이 됩니다. 함수형 프로그래밍 언어에서 함수는 값으로 취급되며, 필드에 저장되거나 다른 함수의 인자로 전달되거나, 함수에서 반환될 수 있습니다. 
+ * 이렇게 이른바 일급 함수(first-class functions)는 언어마다 “클로저(closures)”, “람다(lambdas)”, “익명 함수(anonymous functions)” 등 여러 이름으로 불립니다.
+ * 함수형 프로그래밍을 제대로 이해하고 활용하기 위해서는, 특히 스칼라를 비롯한 어떤 프로그래밍 언어를 다룸에 있어서도, 함수를 올바르게 이해하고 사용할 수 있어야 합니다.
+ * 이 모듈에서는 “값으로서의 함수” 개념을 배우고, 함수를 직접 만들고, 변환하고, 합성하며, 타입을 지정하는 법을 익히게 될 것입니다.
  */
 package net.degoes.lambdas
 
@@ -27,11 +28,10 @@ object Lambdas extends ZIOSpecDefault {
         /**
          * EXERCISE
          *
-         * Create a lambda which squares its argument and store the lambda into
-         * the `square` variable
+         * 인자를 제곱하는 람다를 생성하고, 이를 `square` 변수에 저장하세요.
          */
         test("square") {
-          val square: Int => Int = ???
+          val square: Int => Int = ??? // TODO
 
           assertTrue(square(3) == 9)
         } @@ ignore +
@@ -40,29 +40,31 @@ object Lambdas extends ZIOSpecDefault {
             /**
              * EXERCISE
              *
-             * Create a lambda with two arguments, which adds them together, and
-             * store the lambda into the `plus` variable.
+             * 두 인자를 더하는 람다를 생성하고, 이를 `plus` 변수에 저장하세요.
              */
-            val plus: (Int, Int) => Int = ???
+            val plus: (Int, Int) => Int = ??? // TODO
 
             assertTrue(plus(2, 2) == 4)
           } @@ ignore +
           /**
            * EXERCISE
-           *
-           * Use the `_` to create a lambda that adds one to its argument.
+           * 
+           * 스칼라에서 `_`는 와일드카드로 쓰입니다. 문맥에 따라 다양한 의미로 사용됩니다.
+           * `.map(_.a)`는 `map(x => x.a)`와 동일합니다.
+           * `_`를 사용하여 인자에 1을 더하는 람다를 생성하고, 이를 `addTwo` 변수에 저장하세요.
            */
           test("underscore") {
-            val addTwo: Int => Int = ???
+            val addTwo: Int => Int = ??? // TODO
 
             assertTrue(addTwo(2) == 4)
           } @@ ignore +
           /**
            * EXERCISE
-           *
-           * Use `Function#andThen` to compose two of the following functions
-           * together to create a composite function that counts the number of
-           * digits inside an integer.
+           * 
+           * 합성 함수란, 하나의 함수 실행 결과를 다음 함수 입력으로 넘겨 연결해 주는 함수입니다.
+           * 스칼라에서는 `Function#andThen`을 사용하여 합성 함수를 생성할 수 있습니다.
+           * `Function#andThen`을 사용하여 두 함수를 합성하여, 정수 내부의 숫자 개수를 세는 합성 함수를 생성하세요.
+           * f andThen g === g(f(x)) 와 동일합니다.
            */
           test("andThen") {
             val convertToString: Int => String = _.toString
@@ -70,18 +72,16 @@ object Lambdas extends ZIOSpecDefault {
 
             val _ = (convertToString, countLength)
 
-            val numberOfDigits: Int => Int = ???
+            val numberOfDigits: Int => Int = ??? // TODO
 
             assertTrue(numberOfDigits(123) == 3)
           } @@ ignore +
           /**
            * EXERCISE
            *
-           * Use `Function#compose` to compose two of the following functions
-           * together to create a composite function that counts the number of
-           * digits inside an integer.
-           *
-           * Which one do you prefer, `compose` or `andThen`?
+           * 합성 함수를 만드는 다른 함수로는 `Function#compose`가 있습니다.
+           * `Function#compose`와 `Function#andThen`의 차이점은 순서입니다.
+           * f compose g === f(g(x)) 와 동일합니다.
            */
           test("compose") {
             val convertToString: Int => String = _.toString
@@ -89,15 +89,15 @@ object Lambdas extends ZIOSpecDefault {
 
             val _ = (convertToString, countLength)
 
-            val numberOfDigits: Int => Int = ???
+            val numberOfDigits: Int => Int = ??? // TODO
 
             assertTrue(numberOfDigits(123) == 3)
           } @@ ignore +
           /**
            * EXERCISE
            *
-           * Use `identity` to create a function that returns its same string
-           * argument.
+           * `identity`는 자기 자신을 반환하는 함수를 생성합니다.
+           * `identity`를 사용하여 자기 자신을 반환하는 함수를 생성하세요.
            */
           test("identity") {
             val sameString: String => String = ???
@@ -107,37 +107,31 @@ object Lambdas extends ZIOSpecDefault {
           /**
            * EXERCISE
            *
-           * Use `Function.const` to create a function that always returns 42,
-           * regardless of whichever string argument is passed.
+           * `Function.const`는 항상 인자를 무시하고 주어진 입력을 반환합니다.
+           * `Function.const`를 사용하여 항상 42를 반환하는 함수를 생성하세요.
            */
           test("const") {
-            val answer: String => Int = ???
+            val answer: String => Int = ??? // TODO
 
             assertTrue(answer("foo") == answer("bar") && answer("foobar") == 42)
           } @@ ignore +
           /**
            * EXERCISE
            *
-           * Create a function which returns a function. The returned function
-           * should prepend the specified number of spaces to the string that it
-           * is passed.
+           * 주어진 문자열 앞에 입력된 수만큼의 공백을 추가하는 함수를 생성하세요.
            */
           test("prependSpace") {
-            val prependSpace: Int => (String => String) = ???
+            val prependSpace: Int => (String => String) = ??? // TODO
 
             assertTrue(prependSpace(5)("foo") == "     foo")
           } @@ ignore +
           /**
            * EXERCISE
            *
-           * Create a function that returns a function transformer. The function
-           * transformer should take a `String => String` function, and then
-           * return a new `String => String` function that will repeat the original
-           * function the specified number of times.
+           * 주어진 문자열에 입력된 수만큼 문자열을 반복하여 반환하는 함수를 생성하세요.
            */
           test("repeat") {
-            val repeat: Int => (String => String) => (String => String) =
-              ???
+            val repeat: Int => (String => String) => (String => String) = ??? // TODO
 
             assertTrue(repeat(5)(_ + ".")("Coming soon") == "Coming soon.....")
           } @@ ignore
@@ -147,97 +141,101 @@ object Lambdas extends ZIOSpecDefault {
           /**
            * EXERCISE
            *
-           * Determine the type of `f`, and place this (function) type in the
-           * space provided. If you are correct, then the test will compile.
+           * f의 결괏값에 해당하는 타입을 정의하세요.
            */
           test("example 1") {
             val f = (x: Int) => x * x
 
-            type Type = f.type // EDIT HERE
+            type Type = f.type // TODO: EDIT HERE
 
             assertTypeEquals[f.type, Type]
           } @@ ignore +
             /**
              * EXERCISE
              *
-             * Determine the type of `f`, and place this (function) type in the
-             * space provided. If you are correct, then the test will compile.
+             * f의 결괏값에 해당하는 타입을 정의하세요.
              */
             test("example 2") {
               val f = (x: Int, y: Int) => x + y
 
-              type Type = f.type // EDIT HERE
+              type Type = f.type // TODO: EDIT HERE
 
               assertTypeEquals[f.type, Type]
             } @@ ignore +
             /**
              * EXERCISE
              *
-             * Determine the type of `f`, and place this (function) type in the
-             * space provided. If you are correct, then the test will compile.
+             * f의 결괏값에 해당하는 타입을 정의하세요.
              */
             test("example 3") {
               val f = (t: (Int, Int)) => t._1 + t._2
 
-              type Type = f.type // EDIT HERE
+              type Type = f.type // TODO: EDIT HERE
 
               assertTypeEquals[f.type, Type]
             } @@ ignore +
             /**
              * EXERCISE
              *
-             * Determine the type of `f`, and place this (function) type in the
-             * space provided. If you are correct, then the test will compile.
+             * f의 결괏값에 해당하는 타입을 정의하세요.
              */
             test("example 4") {
               val f = (x: Int) => (y: Int) => x + y
 
-              type Type = f.type // EDIT HERE
+              type Type = f.type // TODO: EDIT HERE
 
               assertTypeEquals[f.type, Type]
             } @@ ignore +
             /**
              * EXERCISE
              *
-             * Determine the type of `f`, and place this (function) type in the
-             * space provided. If you are correct, then the test will compile.
+             * f의 결괏값에 해당하는 타입을 정의하세요.
              */
             test("example 5") {
               val f = (x: Int) => (g: Int => Int) => g(x)
 
-              type Type = f.type // EDIT HERE
+              type Type = f.type // TODO: EDIT HERE
 
               assertTypeEquals[f.type, Type]
             } @@ ignore
         } +
         suite("partiality") {
+          /**
+           * `PartialFunction`는 부분 함수를 정의하는 타입입니다.
+           * 부분 함수란, 정의된 입력 값들에만 동작하는 함수입니다.
+           *  주어진 인자에 대해 함수가 정의되어 있는지 검사할 수 있는 `isDefinedAt` 메서드를 가집니다.
+           * ```scala
+           * val pf: PartialFunction[Int] = {
+           *   case x if x > 0 => x
+           * }
+           * ```
+           * 위 예시에서 `pf`는 양수일 때만 정의됩니다. 따라서 `pf.isDefinedAt(0)`은 `false`를 반환합니다.
+           */
 
           /**
            * EXERCISE
            *
-           * Define a `divide` partial function that is only defined when the
-           * second component of the tuple is non-zero.
+           * 튜플의 두 번째 요소가 0이 아닐 때만 정의되는 부분 함수 `divide`를 정의하세요.
            */
           test("divide") {
             val divide: PartialFunction[(Int, Int), Int] =
-              ???
+              ??? // TODO
 
             assertTrue(!divide.isDefinedAt((42, 0)))
           } @@ ignore +
             /**
              * EXERCISE
              *
-             * Define a `divideOption` method by using the
-             * `PartialFunction#lift` method.
+             * `PartialFunction#lift` 메서드를 사용하여 `oddOption` 메서드를 정의하세요.
              */
             test("lift") {
-              val divide: PartialFunction[(Int, Int), Int] = {
-                case (x, y) if y != 0 => x / y
+              val odd: PartialFunction[Int, Int] = {
+                case x if x % 2 == 1 => x
               }
 
-              val _ = divide
+              val _ = odd
 
-              def divideOption: ((Int, Int)) => Option[Int] = ???
+              def divideOption: ((Int, Int)) => Option[Int] = ??? // TODO
 
               assertTrue(divideOption((42, 0)) == None)
             } @@ ignore
@@ -246,15 +244,15 @@ object Lambdas extends ZIOSpecDefault {
 }
 
 /**
- * Parsers are a great example of the power of lambdas. A parser can be viewed
- * as nothing more than a lambda. Functions can construct or combine parsers,
- * providing the ability to compositionally specify how to parse any type of
- * data.
+ * 파서는 람다가 가진 강력함을 잘 보여주는 예시입니다.
+ * 파서는 본질적으로 람다 그 자체로 볼 수 있습니다.
+ * 함수는 파서를 생성하거나 결합할 수 있어, 어떤 형태의 데이터든 파싱하는 방법을 구성적으로 정의할 수 있게 해줍니다.
  *
- * In this graduation project, you will gain experience constructing lambdas,
- * including higher-order lambdas which themselves accept and return lambdas.
+ * 이 마지막 프로젝트에서 여러분은 람다를 직접 만들어보는 경험을 하게 될 것입니다.
+ * 여기에는 람다를 인자로 받고, 또 람다를 반환하는 고차 람다를 구성하는 것도 포함됩니다.
  */
 object LambdasGraduation extends ZIOAppDefault {
+  // 각 String, (String, A)의 의미를 생각해 보세요.
   type Parser[+A] = String => Either[String, (String, A)]
 
   object Parser {
@@ -262,34 +260,30 @@ object LambdasGraduation extends ZIOAppDefault {
     /**
      * EXERCISE
      *
-     * Implement a parser that succeeds with the specified value, but does not
-     * consume any input.
+     * 주어진 값으로 성공하는 파서를 생성하세요. 입력 이외의 다른 값을 소모하지 않습니다.
      */
-    def succeed[A](a: => A): Parser[A] = ???
+    def succeed[A](a: => A): Parser[A] = ??? // TODO
 
     /**
      * EXERCISE
      *
-     * Implement a parser that fails with the specified message, and does not
-     * consume any input.
+     * 주어진 메시지로 실패하는 파서를 생성하세요. 입력 이외의 다른 값을 소모하지 않습니다.
      */
-    def fail(message: => String): Parser[Nothing] = ???
+    def fail(message: => String): Parser[Nothing] = ??? // TODO
 
     /**
      * EXERCISE
      *
-     * Implement a parser that consumes any character, or fails if there are
-     * no characters left to consume.
+     * 입력에서 한 글자를 읽는 파서를 생성하세요. 입력이 없으면 실패합니다.
      */
-    def anyChar: Parser[Char] = ???
+    def anyChar: Parser[Char] = ??? // TODO
 
     /**
      * EXERCISE
      *
-     * Implement a parser that parses only the specified character, or fails
-     * with a message indicating which character was expected.
+     * 주어진 문자만 파싱하는 파서를 생성하세요. 입력이 없거나 주어진 문자가 아니면 실패합니다.
      */
-    def char(char: Char): Parser[Unit] = ???
+    def char(char: Char): Parser[Unit] = ??? // TODO
   }
 
   implicit class ParserExtensionMethods[A](self: Parser[A]) {
@@ -298,12 +292,14 @@ object LambdasGraduation extends ZIOAppDefault {
 
     /**
      * EXERCISE
+     * 
+     * `Function#flatMap`이란, 상자를 열어 값을 꺼내서 처리하고, 다시 상자에 넣어 주는 것과 같습니다.
      *
-     * Implement a function that can feed the output value of this parser
-     * into the provided callback, which can return a new parser which
-     * will be fed the leftover input of this parser.
+     * 이 파서의 출력 값을 콜백 함수에 전달하고,
+     * 이 콜백이 새 파서를 반환하면,
+     * 그 새 파서가 이 파서가 남긴 입력을 이어서 처리할 수 있도록 하는 함수를 구현하세요.
      */
-    def flatMap[B](f: A => Parser[B]): Parser[B] = ???
+    def flatMap[B](f: A => Parser[B]): Parser[B] = ??? // TODO
 
     def ~[B](that: => Parser[B]): Parser[(A, B)] =
       self.flatMap(a => that.map(b => (a, b)))
@@ -315,10 +311,10 @@ object LambdasGraduation extends ZIOAppDefault {
     /**
      * EXERCISE
      *
-     * Implement a function that will try to parse using the left-hand side,
-     * but if that fails, it will try to parse using the right-hand side.
+     * 좌측 파서를 사용해 파싱을 시도하되,
+     * 만약 실패하면 우측 파서를 사용해 파싱을 시도하는 함수를 구현하시오.
      */
-    def |(that: => Parser[A]): Parser[A] = ???
+    def |(that: => Parser[A]): Parser[A] = ??? // TODO
 
     def repeat: Parser[List[A]] =
       (self ~ repeat).map {
@@ -351,10 +347,9 @@ object LambdasGraduation extends ZIOAppDefault {
   /**
    * EXERCISE
    *
-   * Implement a function to parse the contents of a CSV file into  alist of
-   * CSV data elements.
+   * 파일 내용을 CSV 데이터 요소의 리스트로 파싱하는 함수를 구현하세요.
    */
-  def parseFile(contents: String): Either[String, List[CSVData]] = ???
+  def parseFile(contents: String): Either[String, List[CSVData]] = ??? // TODO
 
   def run =
     for {
